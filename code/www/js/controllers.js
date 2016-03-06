caae29b09d6a4e668be767e3373e4d8cd9dce7c7
ray-curran <rpcurr@gmail.com>
2016-03-06 14:21:22 -0600

    Add files that work - swiping not present yet

---
+++ code/www/js/controllers.js
@@ -0,0 +1,116 @@
+angular.module('songhop.controllers', ['ionic', 'songhop.services'])
+
+
+/*
+Controller for the discover page
+*/
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+
+
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
+
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  showLoading();
+
+  Recommendations.init()
+    .then(function() {
+      $scope.currentSong = Recommendations.queue[0];
+      Recommendations.playCurrentSong();
+      $scope.song = $scope.currentSong;
+      })
+    .then(function(){
+      hideLoading();
+      $scope.currentSong.loaded = true;
+    })
+
+  $scope.sendFeedback = function(bool) {
+
+    if (bool) User.addSongToFavorites($scope.currentSong);
+
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+    Recommendations.nextSong();
+
+    $timeout(function() {
+
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
+    }, 250);
+
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    });
+  };
+
+  $scope.nextAlbumImg = function() {
+    if (Recommendations.queue.length > 1) {
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  };
+
+
+})
+
+
+/*
+Controller for the favorites page
+*/
+.controller('FavoritesCtrl', function($scope, User, $window) {
+  $scope.favorites = User.favorites;
+  $scope.username = User.username;
+
+  $scope.removeSong = function(song, index) {
+    User.removeSongFromFavorites(song, index);
+  }
+
+  $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
+  }
+
+})
+
+
+/*
+Controller for our tab bar
+*/
+.controller('TabsCtrl', function($scope, User, Recommendations, $window) {
+
+  $scope.favCount = User.favoriteCount;
+
+  $scope.enteringFavorites = function() {
+    User.newFavorites = 0;
+    Recommendations.haltAudio();
+  };
+
+  $scope.leavingFavorites = function() {
+    Recommendations.init();
+  };
+
+  $scope.logout = function() {
+    User.destroySession();
+    $window.location.href = 'index.html';
+  }
+})
+
+.controller('SplashCtrl', function($scope, $state, User) {
+
+  $scope.submitForm = function(username, signingUp) {
+    User.auth(username, signingUp).then(function() {
+      $state.go('tab.discover');
+    }, function() {
+      alert('Hmm... try another username.');
+    });
+  };
+
+});
---
+++ code/www/js/services.js
@@ -0,0 +1,154 @@
+angular.module('songhop.services', ['ionic.utils'])
+.factory('User', function($http, $q, $localstorage, SERVER) {
+
+  var o = {
+    username: false,
+    session_id: false,
+    favorites: [],
+    newFavorites: 0
+  };
+
+  o.auth = function(username, signingUp) {
+    var authRoute;
+
+    if (signingUp) {
+      authRoute = 'signup';
+    } else {
+      authRoute = 'login'
+    }
+
+    return $http.post(SERVER.url + '/' + authRoute, {username: username})
+    .success(function(data){
+      o.setSession(data.username, data.session_id, data.favorites);
+    });
+  };
+
+  o.setSession = function(username, session_id, favorites) {
+    if (username) o.username = username;
+    if (session_id) o.session_id = session_id;
+    if (favorites) o.favorites = favorites;
+
+    $localstorage.setObject('user', {username: username, session_id: session_id})
+  };
+
+  o.checkSession = function() {
+    var defer = $q.defer();
+
+    if (o.session_id) {
+      defer.resolve(true);
+    } else {
+      var user = $localstorage.getObject('user');
+
+      if (user.username) {
+        o.setSession(user.username, user.session_id);
+        o.populateFavorites().then(function(){
+          defer.resolve(true);
+        });
+      } else {
+        defer.resolve(false);
+      }
+    }
+
+    return defer.promise;
+  };
+
+  o.destroySession = function() {
+    $localstorage.setObject('user', {});
+    o.username = false;
+    o.session_id =false;
+    o.favorites =[];
+    o.newFavorites = 0;
+  }
+
+  o.addSongToFavorites = function(song) {
+    if (!song) return false;
+
+    o.favorites.unshift(song);
+    o.newFavorites++;
+
+    return $http.post(SERVER.url + '/favorites', {session_id: o.session_id, song_id: song.song_id});
+  };
+
+  o.removeSongFromFavorites = function(song, index) {
+    if (!song) return false;
+
+    o.favorites.splice(index, 1);
+
+    return $http({
+      method: 'DELETE',
+      url: SERVER.url + '/favorites',
+      params:{session_id: o.session_id, song_id: song.song_id}
+    });
+  };
+
+  o.populateFavorites = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/favorites',
+      params: { session_id: o.session_id }
+    }).success(function(data) {
+      o.favorites = data;
+    });
+  }
+
+  o.favoriteCount = function() {
+    return o.newFavorites;
+  }
+
+  return o;
+
+})
+.factory('Recommendations', function($http, SERVER, $q) {
+  var o ={
+    queue: []
+  };
+
+  var media;
+
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data) {
+      o.queue = o.queue.concat(data);
+    })
+  };
+
+  o.nextSong = function(){
+    o.queue.shift();
+
+    o.haltAudio();
+
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+  };
+
+  o.playCurrentSong = function() {
+    var defer = $q.defer();
+
+    media = new Audio(o.queue[0].preview_url);
+
+    media.addEventListener('loadeddata', function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  };
+
+  o.haltAudio = function() {
+    if (media) media.pause();
+  };
+
+  o.init = function() {
+    if(o.queue.length === 0) {
+      return o.getNextSongs();
+    } else {
+      return o.playCurrentSong();
+    }
+  }
+
+  return o;
+});
